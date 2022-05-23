c41fe4332030f63065fb8fe6153c2bafadd159bf
ShineTiger <555ysng@gmail.com>
2022-05-23 11:50:37 +0900

    고량주 4주차 과제

--- add.js
+++ add.js
@@ -4,29 +4,37 @@ const handleSaveBtn = document.querySelector("#save-btn");
 const handleCloseBtn = document.querySelector("#close-btn");
 
 const viewModal = () => {
-    handleInputModal.style.display = "block"
-    
-}
+  handleInputModal.style.display = "block";
+};
 
 const unViewModal = () => {
-    handleInputModal.style.display = "none"
+  handleInputModal.style.display = "none";
+};
+
+const handleSaveItem = (name, image, content) => {
+  const list = document.querySelector("ul");
+  const cardList = document.querySelector("#cards");
+  const newCard = document.createElement("div");
+  const listItem = document.createElement("li");
+  newCard.classList.add("card");
+  newCard.querySelector("#viewimg").src= `${image}`;
+  newCard.querySelector("span").innerText= `${content}`;
+
+  cardList.appendChild(newCard);
+  list.innerText= `${name}`;
+  list.appendChild(listItem)
+
+};
+
+const handleSaveList = (event) => {
+  unViewModal();
+  event.preventDefault();
+  const name = document.querySelector("#name").value;
+  const image = document.querySelector("img").value;
+  const content = document.querySelector("#contents").value;
+  handleSaveItem(name, image, content);
 }
 
-function handleAddList() {
-    
-    
-    unViewModal();
-}
-
-const handleSubmitBtn = (e) => {
-    e.preventDefault();  
-    const name = document.querySelector("#name").value;
-    const img = document.querySelector("#image").value;
-    const content = document.querySelector("#contents").value;
-}
-
-
-
-handleAddBtn.addEventListener("click",viewModal)
-handleCloseBtn.addEventListener("click",unViewModal)
-handleSaveBtn.addEventListener("click",handleAddList)
+handleAddBtn.addEventListener("click", viewModal);
+handleCloseBtn.addEventListener("click", unViewModal);
+handleSaveBtn.addEventListener("click", handleSaveList);
